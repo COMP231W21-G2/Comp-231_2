16fd354bfed09c1bf57c616c2386915e7ca2271a
rubleen <72541067+rubleen@users.noreply.github.com>
2021-04-16 23:03:06 -0400

    Add files via upload

---
+++ get appointment table.sql
@@ -0,0 +1,23 @@
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE procedure [SHP].[get_appointments](@patientId int)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF(@patientId IS NULL OR @patientId =0)
+	BEGIN
+		SELECT *
+		FROM SHP.[schedule_appointment];
+	END
+	ELSE
+	BEGIN
+		SELECT *
+		FROM SHP.[S	P]
+		WHERE patient_id = @patientId;
+	END	
+END
